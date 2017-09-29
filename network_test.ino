d76f5c27c71865ed28274f45daabadd297e78b56
Abhi <abhi2decide@gmail.com>
2017-09-29 14:43:21 +0530

    Network For Data Transmisson
    Uses RF24Network library 
    Works only with pcb nrf chips

---
+++ network_test.ino
@@ -0,0 +1,69 @@
+#include <RF24_config.h>
+#include <printf.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include <RF24Network.h>
+#include <RF24Network_config.h>
+#include <Sync.h>
+
+#include <SPI.h>
+
+//WORKING
+
+RF24 radio(8,7);
+RF24Network network(radio);
+
+//addresses of all nodes in the network are octal numbers hence the 0 in the beginning
+uint16_t node0 = 011;  
+uint16_t node1 = 01;
+uint16_t node2 = 00;
+uint16_t node3 = 02;
+uint16_t node4 = 012;
+//uint16_t node5 = 022;
+//end
+
+uint16_t this_node = 01;
+
+uint16_t reciever_node = 00;
+uint16_t transmitter_node = 01;
+
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("Code to Set up a RF24 Network with 5 nodes");
+  
+  SPI.begin();
+  radio.begin();
+  network.begin(90,this_node);
+  
+}
+
+void loop() {
+
+  network.update();
+  
+  if(this_node == transmitter_node){
+    Serial.println("Transmitter Node");
+    char text[32] = "hello there";
+    RF24NetworkHeader header(reciever_node);
+    bool ok = network.write(header,text,sizeof(text));
+    if(ok){
+      Serial.println("Data Transmitted");
+    }
+    else{
+      Serial.println("failed");
+    }
+  }
+  
+  if(this_node == reciever_node){
+    Serial.println("reciever node");
+    while(network.available()){
+      RF24NetworkHeader header;
+      char text[32] =" ";
+      network.read(header,text,sizeof(text));
+      Serial.println(text);
+    }
+  }
+  
+}
